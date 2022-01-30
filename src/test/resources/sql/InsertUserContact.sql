5dd5567168fefa76824ebf0d288bc1605996fea6
Konstantin Ivanin <ivanin.k@gmail.com>
2022-01-30 08:40:14 +0300

    FMS-7: write tests

---
+++ src/test/resources/sql/InsertUserContact.sql
@@ -0,0 +1,7 @@
+insert into user_contact(user_id, user_contact_type_id, name, is_active)
+values (1, 1, 'e@mail.ru', true),
+       (1, 2, 'telegram', true),
+       (1, 3, 'whatsapp', true),
+       (2, 1, 'some@mail.com', true),
+       (2, 2, 'telegram', true),
+       (2, 3, 'WA', false);
